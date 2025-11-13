99119b5734a88c6098542656fa2f6a9c1d3b01ed
Kevin Kozar <kevin.kozar@avl.com>
2025-11-13 17:01:11 +0100

    ADD | UI instead of raw json return

--- bearingpoint-ci-cd-demo.Api/Program.cs
+++ bearingpoint-ci-cd-demo.Api/Program.cs
@@ -1,16 +1,16 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
-app.MapGet("/", () => Results.Ok(new
+app.MapGet("/api/info", () => Results.Ok(new
 {
     message = "Hello BearingPoint CI/CD!",
     environment = builder.Environment.EnvironmentName,
     timestamp = DateTime.Now
 }));
+
 
 app.MapGet("/health", () => Results.Ok(new
 {
@@ -18,11 +18,9 @@ app.MapGet("/health", () => Results.Ok(new
     timestamp = DateTime.Now
 }));
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
