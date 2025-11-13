91cefd3c0c107172cafb99c14c56c50514e2280c
Kevin Kozar <kevin.kozar@avl.com>
2025-11-13 18:05:40 +0100

    UPDATE | added FAQ to root page

--- bearingpoint-ci-cd-demo.Api/Pages/Index.cshtml.cs
+++ bearingpoint-ci-cd-demo.Api/Pages/Index.cshtml.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
 
 namespace bearingpoint_ci_cd_demo.Pages
 {
@@ -12,9 +13,25 @@ namespace bearingpoint_ci_cd_demo.Pages
             _logger = logger;
         }
 
+        public record FaqItem(string Question, string Answer);
+
+        public List<FaqItem> Faqs { get; private set; } = new();
+
         public void OnGet()
         {
-
+            Faqs = new()
+            {
+                new("Wer bin ich?",
+                    "Ich bin Kevin, Softwareentwickler mit Fokus auf .NET, Azure und CI/CD."),
+                new("Warum dieses Projekt?",
+                    "Dieses Projekt ist meine Bewerbungs-Demo. Es zeigt CI/CD, Azure-Deployment, Healthchecks und ein eigenes Frontend."),
+                new("Welche Technologien nutze ich?",
+                    "ASP.NET Core, C#, Razor Pages, Azure App Service, GitHub Actions und Ansible."),
+                new("Was zeichnet mich aus?",
+                    "Ich arbeite lösungsorientiert, strukturiert und automatisiere gern wiederkehrende Abläufe."),
+                new("Wie kann man mich erreichen?",
+                    "E-Mail: kevinkozar2005@gmail.com · LinkedIn: https://www.linkedin.com/in/kevin-kozar-5a677630b/")
+            };
         }
     }
 }
